5c51de1e818b3f9325f41c3fe77df465b3d1c6ec
deng Mingwei <3561294402@qq.com>
2022-02-17 12:14:13 +0800

    OAuth2.0 权限控制

--- source/src/main/java/com/study/controller/ProductController.java
+++ source/src/main/java/com/study/controller/ProductController.java
@@ -1,14 +1,23 @@
 package com.study.controller;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
-    @GetMapping
+    @Secured("ROLE_PRODUCT")
+    @GetMapping("/a")
     public String findAll() {
         return "查询产品列表成功！";
+    }
+
+    @Secured("PRODUCT")//下面的不允许访问
+    @GetMapping("/b")
+    public String get() {
+        return "get成功！";
     }
 }
